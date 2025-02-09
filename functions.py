d3e7fa1e8a9b973abfc498f9441db0add1f580b5
Tohqa <goraktymofii@gmail.com>
2025-02-09 18:20:29 +0200

    додано функцію виведення

--- functions.py
+++ functions.py
@@ -5,3 +5,9 @@ def read_znach(filename):
     except FileNotFoundError:
         print(f"Файл {filename} не знайдено")
         return ''
+
+
+def print_employee_info(name, post, file_write='result.txt'):
+    with open(file_write, 'w', encoding='utf-8') as file:
+        for name, post in zip(name, post):
+            file.write(f'Співробітник {name}, посада - {post}\n')
